2ce8964197356f5d836015426d4ddc80903ec254
Spikuser <id270937@gmail.com>
2023-06-24 19:58:43 +0300

    added couple of blocks

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,21 @@
 $(function(){
-
+    $('select#cities').on('change', function() {
+        $('input[name="check__form-input--room"]').val(this.value);
+      });
+      $('.customers-slider__inner').slick({
+        dots: true,
+        arrows:false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      });
+      $('.blog-slider__inner').slick({
+        dots: true,
+        arrows:false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      });
 });
